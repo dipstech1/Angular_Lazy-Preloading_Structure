02fea46e84df5eb0ff108fb1c117095cc308058b
SOURADIP PANJA <souradippanja2@gmail.com>
2019-04-07 23:15:48 +0530

    Adding shared module

--- src/app/index/index.module.ts
+++ src/app/index/index.module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms'
-import {RouterModule} from '@angular/router'
-
 
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
-
+import {SharedModule} from './../shared/shared.module'
 import {IndexRoutingModule} from './index-routing.module'
 
 @NgModule({
   declarations: [LoginComponent, SignupComponent, ForgotpasswordComponent],
   imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    IndexRoutingModule
+    IndexRoutingModule,
+    SharedModule
   ]
 })
 export class IndexModule { }
